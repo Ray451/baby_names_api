55f4fe8eed953e10efd0a44827000fe93eeeecc8
Ray451 <kainmr410@gmail.com>
2023-02-05 23:36:21 -0800

    prevent to duplicate user name

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -2,5 +2,6 @@ class Baby < ApplicationRecord
   belongs_to :baby_list
 
   validates :name, presence: true
+  validates_uniqueness_of :name, scope: :baby_list_id
   validates :baby_list_id, presence: true
 end
